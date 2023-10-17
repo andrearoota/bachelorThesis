af0daa7506a53286d57fb5f9e46e75c83592a4d0
Andrea Rota <andrea.rota.98@gmail.com>
2023-10-17 23:09:14 +0200

    Integrate conference rating

--- scraper/src/aggregateDocuments.py
+++ scraper/src/aggregateDocuments.py
@@ -25,38 +25,50 @@ class AggregateDocuments:
     def get_authors_from_authors_collection(self, spark):
         return spark.read.format('mongodb').option('database', 'clusterScopus').option('collection', self.COLLECTION_AUTHORS).load()
 
-    def split_author_ids(self, df):
-        return df.withColumn('author_ids_array', split(col('author_ids'), ';'))
+    def convert_string_column_to_int(self, spark_collection, column_name):
+        return spark_collection.withColumn(column_name, spark_collection[column_name].cast('int'))
 
-    def aggregate_main_authors(self, df_authors, df_abstracts):
-        df_main_authors = df_abstracts. \
+    def split_author_ids(self, spark_collection):
+        return spark_collection.withColumn('author_ids_array', split(col('author_ids'), ';'))
+
+    def join_conference_rating(self, spark, spark_abstracts):
+        df_conference_rating = pd.read_csv('/Users/andrearota/Documents/GitHub/bachelorThesis/scraper/data/GII-GRIN-SCIE-Conference-Rating-24-ott-2021.csv', on_bad_lines='skip', sep=';')
+        df_conference_rating = df_conference_rating[df_conference_rating['GGS Rating'] != 'Work in Progress']
+        df_conference_rating = spark.createDataFrame(df_conference_rating)
+        df_conference_rating = df_conference_rating.withColumnRenamed('GGS Rating','GGS_Rating')
+
+        return spark_abstracts.alias('base').join(df_conference_rating.alias('external'), lower(col('publicationName')).contains(lower(df_conference_rating['Title'])), 'left') \
+            .selectExpr('base.*', 'external.GGS_Rating')
+
+    def aggregate_main_authors(self, spark_authors, spark_abstracts):
+        spark_main_authors = spark_abstracts. \
             where(col('author_ids_array') != array()). \
-            withColumn('main_author', df_abstracts['author_ids_array'].getItem(0)). \
+            withColumn('main_author', spark_abstracts['author_ids_array'].getItem(0)). \
             groupBy(col('main_author').alias('main_author_ref')). \
-            agg(collect_list(struct(df_abstracts.columns)).alias('main_author'))
+            agg(collect_list(struct(spark_abstracts.columns)).alias('main_author'))
         
-        df_authors = df_authors.join(df_main_authors, split(df_authors.coredata['dc:identifier'], ':')[1] == df_main_authors.main_author_ref, 'left') \
+        spark_authors = spark_authors.join(spark_main_authors, split(spark_authors.coredata['dc:identifier'], ':')[1] == spark_main_authors.main_author_ref, 'left') \
             .drop('main_author_ref')
         
-        return df_authors
+        return spark_authors
 
-    def aggregate_coauthors(self, df_authors, df_abstracts):
-        df_coauthors = df_abstracts. \
+    def aggregate_coauthors(self, spark_authors, spark_abstracts):
+        spark_coauthors = spark_abstracts. \
             where(col('author_ids_array') != array()). \
             withColumn('author_ids_array', slice('author_ids_array', 2, size('author_ids_array'))). \
             withColumn('author_ids_array', explode('author_ids_array')). \
             groupBy(col('author_ids_array').alias('coauthor_ref')). \
-            agg(collect_list(struct(df_abstracts.columns)).alias('coauthor'))
+            agg(collect_list(struct(spark_abstracts.columns)).alias('coauthor'))
         
-        df_authors = df_authors.join(df_coauthors, split(df_authors.coredata['dc:identifier'], ':')[1] == df_coauthors.coauthor_ref, 'left') \
+        spark_authors = spark_authors.join(spark_coauthors, split(spark_authors.coredata['dc:identifier'], ':')[1] == spark_coauthors.coauthor_ref, 'left') \
             .drop('coauthor_ref')
         
-        df_authors = df_authors.select('*', struct('main_author', 'coauthor').alias('articles')). \
+        spark_authors = spark_authors.select('*', struct('main_author', 'coauthor').alias('articles')). \
             drop('main_author'). \
             drop('coauthor'). \
             drop('author_ids_array')
         
-        return df_authors
+        return spark_authors
 
     def aggregate(self):
         spark = SparkSession.builder \
@@ -67,24 +79,32 @@ class AggregateDocuments:
             .getOrCreate()
 
         print('Get all authors from authors')
-        df_authors = self.get_authors_from_authors_collection(spark)
+        spark_authors = self.get_authors_from_authors_collection(spark)
 
-        if df_authors.isEmpty():
+        if spark_authors.isEmpty():
             print('there are zero authors')
             return
 
         print('Get all abstracts from abstracts')
-        df_abstracts = self.get_abstracts_from_abstracts_collection(spark)
+        spark_abstracts = self.get_abstracts_from_abstracts_collection(spark)
 
-        if df_abstracts.isEmpty():
+        if spark_abstracts.isEmpty():
             print('there are zero abstracts')
             return
 
-        df_abstracts_split_ids = self.split_author_ids(df_abstracts)
-        df_authors = self.aggregate_main_authors(df_authors, df_abstracts_split_ids)
-        df_authors = self.aggregate_coauthors(df_authors, df_abstracts_split_ids)
+        print('Convert some string fields to int')
+        spark_authors = self.convert_string_column_to_int(spark_authors, 'h-index')
+        spark_authors = self.convert_string_column_to_int(spark_authors, 'coauthor-count')
+        spark_abstracts = self.convert_string_column_to_int(spark_abstracts, 'author_count')
 
-        df_authors.write \
+        print('Search GGS rating conference')
+        spark_abstracts = self.join_conference_rating(spark, spark_abstracts)
+
+        spark_abstracts_split_ids = self.split_author_ids(spark_abstracts)
+        spark_authors = self.aggregate_main_authors(spark_authors, spark_abstracts_split_ids)
+        spark_authors = self.aggregate_coauthors(spark_authors, spark_abstracts_split_ids)
+
+        spark_authors.write \
             .format('mongodb') \
             .option('database', 'clusterScopus').option('collection', self.COLLECTION_AGGREGATE) \
             .mode('overwrite') \
