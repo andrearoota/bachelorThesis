e144a919c4666780ab3287df90095aa23560c25e
Andrea Rota <andrea.rota.98@gmail.com>
2023-10-29 22:50:44 +0100

    Enhancements to the ability to add new analyses Minor fix

---
+++ backend/flask/analyses.py
@@ -0,0 +1,213 @@
+import pandas as pd
+from numpy import nan
+from flask_pymongo import PyMongo
+from bson.json_util import dumps, loads
+from datetime import datetime
+
+class analyses:
+    def __init__(self, mongo, h_index = 0):
+        self.mongo = mongo
+        self.h_index = h_index
+
+    def average_coauthors_variation_after_years(self):
+        filter={
+            '$expr': {
+                '$gte': [
+                    '$h-index',
+                    self.h_index
+                ]
+            }
+        }
+        project={
+            '_id': 0, 
+            'articles': -1, 
+            'author-profile.publication-range.@start': -1
+        }
+
+        df_filtered = self.mongo.db.collectionAuthorsAggregate.find(filter=filter,projection=project)
+        df_filtered = pd.DataFrame(list(df_filtered))
+
+        def extract_data(row):
+            career_start_year = int(row['author-profile']['publication-range']['@start'])
+            df_counter = pd.DataFrame(columns=['years_since_career_start', 'coauthors_count'])
+
+            for type in ['main_author', 'coauthor']:
+                if row.articles[type] is not None:
+                    df_counter_temp = pd.DataFrame(columns=['years_since_career_start', 'coauthors_count'])
+                    df = pd.DataFrame(row.articles[type])
+                    df_counter_temp['coauthors_count'] = df['author_count']
+                    df['coverDate'] = pd.to_datetime(df['coverDate'])
+                    df_counter_temp['years_since_career_start'] = df['coverDate'].dt.year - career_start_year
+                    df_counter = pd.concat([df_counter, df_counter_temp], ignore_index=True)
+
+            return df_counter
+
+        df_filtered = df_filtered.apply(lambda row : extract_data(row), axis=1)
+        df_filtered = pd.concat(df_filtered.tolist(), ignore_index=True)
+        df_filtered = df_filtered.groupby(by='years_since_career_start', as_index=False).mean()
+
+        return dumps(
+            {
+                'correlation': df_filtered['years_since_career_start'].corr(df_filtered['coauthors_count']),
+                'avg_coauthors_career': loads(df_filtered.to_json(orient='records'))
+            }
+        )
+
+    def coauthors_impact_on_citations_and_hindex(self):
+        project={
+        '_id': 0, 
+        'author_count': 1,
+        'citedby_count': 1
+        }
+        df_filtered =self.mongo.db.collectionAbstracts.find(projection=project)
+        df_filtered = pd.DataFrame(list(df_filtered))
+
+        df_filtered['author_count'].replace('', nan, inplace=True)
+        df_filtered['citedby_count'].replace('', nan, inplace=True)
+        df_filtered.dropna(subset=['author_count', 'citedby_count'], inplace=True)
+        
+        df_filtered = df_filtered.astype({'author_count': 'int', 'citedby_count': 'int'})
+
+        return dumps(
+            {
+                'correlation': df_filtered['author_count'].corr(df_filtered['citedby_count']),
+                'data': loads(df_filtered.drop_duplicates(subset=['author_count', 'citedby_count']).to_json(orient='records'))
+            }
+        )
+
+    def analyze_hindex_influential_articles_timing(self):
+        filter={
+            '$expr': {
+                '$gte': [
+                    '$h-index',
+                    self.h_index
+                ]
+            }
+        }
+
+        project={
+            '_id': 0, 
+            'articles': -1,
+            'h-index': -1 ,
+            'author-profile.publication-range': -1
+        }
+        df_filtered =self.mongo.db.collectionAuthorsAggregate.find(filter=filter,projection=project)
+        df_filtered = pd.DataFrame(list(df_filtered))
+
+        def extract_data(row):
+            h_index = int(row['h-index'])
+            career_start_year = int(row['author-profile']['publication-range']['@start'])
+            career_end_year = int(row['author-profile']['publication-range']['@end'])
+            df_abstracts = pd.DataFrame()
+
+            for type in ['main_author', 'coauthor']:
+                if row.articles[type] is not None:
+                    df_abstracts = pd.concat([df_abstracts, pd.DataFrame(row.articles[type])[['coverDate', 'citedby_count']]], ignore_index=True)
+
+            if not df_abstracts.empty:
+                df_abstracts = df_abstracts.sort_values('citedby_count', ascending=False).head(h_index)
+
+                df_abstracts['coverYear'] = pd.to_datetime(df_abstracts['coverDate'])
+                df_abstracts['years_since_career_start'] = df_abstracts['coverYear'].dt.year - career_start_year
+                max_difference = career_end_year - career_start_year
+                df_abstracts['coefficient'] = df_abstracts['years_since_career_start'] / max_difference
+                return df_abstracts['coefficient'].mean()
+
+            return None
+
+        df_filtered = df_filtered.apply(lambda row : extract_data(row), axis=1)
+        df_filtered = df_filtered.dropna()
+
+        return dumps(
+            {
+                'weight_career_avg': df_filtered.mean(),
+                'weight_career_data': loads(df_filtered.to_json(orient='records'))
+            }
+        )
+
+    def correlation_between_hindex_and_excluded_articles(self):
+        filter={
+            '$expr': {
+                '$gte': [
+                    '$h-index',
+                    self.h_index
+                ]
+            }
+        }
+
+        project={
+            '_id': 0, 
+            'h-index': -1,
+            'document-count': '$coredata.document-count'
+        }
+        df_filtered =self.mongo.db.collectionAuthorsAggregate.find(filter=filter,projection=project)
+        df_filtered = pd.DataFrame(list(df_filtered))
+
+        return dumps(
+            {
+                'correlation': df_filtered['h-index'].corr(df_filtered['document-count'])
+            }
+        )
+
+    def correlation_between_hindex_and_career_duration(self):
+        filter={
+            'author-profile.publication-range': {
+                '$ne': None
+            }
+        }
+
+        project={
+            '_id': 0, 
+            'h-index': -1,
+            'end': '$author-profile.publication-range.@end',
+            'start': '$author-profile.publication-range.@start'
+        }
+        df_filtered =self.mongo.db.collectionAuthorsAggregate.find(projection=project, filter=filter)
+        df_filtered = pd.DataFrame(list(df_filtered))
+
+        df_filtered['duration_career'] = df_filtered['end'].astype(int) - df_filtered['start'].astype(int)
+
+        return dumps(
+            {
+                'correlation': df_filtered['h-index'].corr(df_filtered['duration_career'])
+            }
+        )
+
+    def citation_count_based_on_conference_ranking(self):
+        filter={
+            '$expr': {
+                '$gte': [
+                    '$h-index',
+                    self.h_index
+                ]
+            }
+        }
+
+        project={
+            '_id': 0, 
+            'h-index': -1,
+            'articles': -1
+        }
+        df_filtered =self.mongo.db.collectionAuthorsAggregate.find(filter=filter,projection=project)
+        df_filtered = pd.DataFrame(list(df_filtered))
+
+        def extract_data(row):
+            df_abstracts = pd.DataFrame()
+
+            for type in ['main_author', 'coauthor']:
+                if row.articles[type] is not None:
+                    df = pd.DataFrame(row.articles[type])
+                    df = df[df['GGS_Rating'].notna()]
+                    df_abstracts = pd.concat([df_abstracts, df], ignore_index=True)
+
+            return df_abstracts
+
+        df_filtered = df_filtered.apply(lambda row : extract_data(row), axis=1)
+        df_filtered = pd.concat(df_filtered.tolist(), ignore_index=True)
+
+        return dumps(
+            {
+                'correlation': df_filtered['GGS_Rating'].astype('category').cat.codes.corr(df_filtered['citedby_count']),
+                'avg_by_rating': loads(df_filtered.groupby(by='GGS_Rating', as_index=False)['citedby_count'].mean().to_json(orient='records'))
+            }
+        )
--- backend/flask/server.py
+++ backend/flask/server.py
@@ -7,266 +7,102 @@ from flask_pymongo import PyMongo
 from bson.json_util import dumps, loads
 from datetime import datetime
 import numpy as np
+from analyses import analyses
+
+ANALYSES = [
+    'average_coauthors_variation_after_years',
+    'coauthors_impact_on_citations_and_hindex',
+    'analyze_hindex_influential_articles_timing',
+    'correlation_between_hindex_and_excluded_articles',
+    'correlation_between_hindex_and_career_duration',
+    'citation_count_based_on_conference_ranking'
+]
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://root:root@host.docker.internal:27017/clusterScopus?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false'
 mongo = PyMongo(app)
 
-@app.route('/api/saved-analysis')
+@app.route('/api/saved_analyses')
 def saved_analysis():
     return dumps(list(mongo.db.cacheAnalysis.find(filter={})))
 
-@app.route('/api/all_analysis')
-def all_analysis():
+
+@app.route('/api/all_analyses')
+def all_analyses():
         
     h_index_threshold = request.args.get('h-index', default=0, type=int)
-    title_analysis = request.args.get('title', default=f'analysis-{h_index_threshold}-hindex', type=str)
+    title_analyses = request.args.get('title', default=f'analysis-{h_index_threshold}-hindex', type=str)
+    analyses_obj = analyses(mongo, h_index_threshold)
+    result_analyses = {}
 
-    result = {
-            'corr_coauthors_career': loads(corr_coauthors_career(h_index_threshold)),
-            'corr_citation_coauthors': loads(corr_citation_coauthors()),
-            'h_index_career': loads(h_index_career(h_index_threshold)),
-            'abstracts_outside_h_index': loads(abstracts_outside_h_index(h_index_threshold)),
-            'corr_career_h_index': loads(corr_career_h_index()),
-            'corr_rating_citedby': loads(corr_rating_citedby(h_index_threshold))
-        }
+    for name_analysis in ANALYSES:
+        result_analyses[name_analysis] = loads(
+            getattr(analyses_obj, name_analysis)())
 
     mongo.db.cacheAnalysis.insert_one({
         'h-index': h_index_threshold,
         'datetime': datetime.now(),
-        'name': title_analysis,
-        'data':result
+        'name': title_analyses,
+        'data': result_analyses
     })
-    return result
 
-# Presi gli autori con h-index > X, come varia il numero medio di coautori dopo Y di anni di carriera?
-@app.route('/api/corr_coauthors_career')
-def corr_coauthors_career(h_index = 0):
+    return result_analyses
 
-    h_index_threshold = request.args.get('h-index', default=h_index, type=int)
 
-    filter={
-        '$expr': {
-            '$gte': [
-                '$h-index',
-                h_index_threshold
-            ]
-        }
-    }
-    project={
-        '_id': 0, 
-        'articles': -1, 
-        'author-profile.publication-range.@start': -1
-    }
+# Given authors with an h-index > X, how does the average number of co-authors change after Y years of their career?
+@app.route('/api/average_coauthors_variation_after_years')
+def average_coauthors_variation_after_years_api():
+    h_index_threshold = request.args.get('h-index', default=0, type=int)
+    analyses_obj = analyses(mongo, h_index_threshold)
 
-    df_filtered = mongo.db.collectionAuthorsAggregate.find(filter=filter,projection=project)
-    df_filtered = pd.DataFrame(list(df_filtered))
+    return analyses_obj.average_coauthors_variation_after_years()
 
-    def extract_data(row):
-        career_start_year = int(row['author-profile']['publication-range']['@start'])
-        df_counter = pd.DataFrame(columns=['years_since_career_start', 'coauthors_count'])
 
-        for type in ['main_author', 'coauthor']:
-            if row.articles[type] is not None:
-                df_counter_temp = pd.DataFrame(columns=['years_since_career_start', 'coauthors_count'])
-                df = pd.DataFrame(row.articles[type])
-                df_counter_temp['coauthors_count'] = df['author_count']
-                df['coverDate'] = pd.to_datetime(df['coverDate'])
-                df_counter_temp['years_since_career_start'] = df['coverDate'].dt.year - career_start_year
-                df_counter = pd.concat([df_counter, df_counter_temp], ignore_index=True)
+# Is there a relationship (and therefore an effect on the h-index) between the number of co-authors on an article and the number of citations?
+# Does having more co-authors influence the number of citations?
+@app.route('/api/coauthors_impact_on_citations_and_hindex')
+def coauthors_impact_on_citations_and_hindex_api():
+    analyses_obj = analyses(mongo)
 
-        return df_counter
+    return obj.coauthors_impact_on_citations_and_hindex()
 
-    df_filtered = df_filtered.apply(lambda row : extract_data(row), axis=1)
-    df_filtered = pd.concat(df_filtered.tolist(), ignore_index=True)
-    df_filtered = df_filtered.groupby(by='years_since_career_start', as_index=False).mean()
 
-    return dumps(
-        {
-            'correlation': df_filtered['years_since_career_start'].corr(df_filtered['coauthors_count']),
-            'avg_coauthors_career': loads(df_filtered.to_json(orient='records'))
-        }
-    )
+# Among authors with an h-index > X, when were the articles that influence the h-index published? At what point in their careers?
+@app.route('/api/analyze_hindex_influential_articles_timing')
+def analyze_hindex_influential_articles_timing_api():
+    h_index_threshold = request.args.get('h-index', default=0, type=int)
+    analyses_obj = analyses(mongo, h_index_threshold)
 
-# Vi è una relazione (e quindi un effetto sul h-index) tra il numero di coautori di un articolo e il numero di citazioni? Avere più coautori influisce sul numero di citazioni?
-@app.route('/api/corr_citation_coauthors')
-def corr_citation_coauthors():
-    project={
-        '_id': 0, 
-        'author_count': 1,
-        'citedby_count': 1
-    }
-    df_filtered = mongo.db.collectionAbstracts.find(projection=project)
-    df_filtered = pd.DataFrame(list(df_filtered))
-
-    df_filtered['author_count'].replace('', np.nan, inplace=True)
-    df_filtered['citedby_count'].replace('', np.nan, inplace=True)
-    df_filtered.dropna(subset=['author_count', 'citedby_count'], inplace=True)
+    return analyses_obj.analyze_hindex_influential_articles_timing()
     
-    df_filtered = df_filtered.astype({"author_count":"int","citedby_count":"int"})
 
-    return dumps(
-        {
-            'correlation': df_filtered['author_count'].corr(df_filtered['citedby_count']),
-            'data': loads(df_filtered.drop_duplicates(subset=['author_count', 'citedby_count']).to_json(orient='records'))
-        }
-    )
+# Given a set of authors with an h-index > X, is there a correlation between the h-index variation
+# and the number of articles not considered for the index calculation?
+@app.route('/api/correlation_between_hindex_and_excluded_articles')
+def correlation_between_hindex_and_excluded_articles_api():
+    h_index_threshold = request.args.get('h-index', default=0, type=int)
+    analyses_obj = analyses(mongo, h_index_threshold)
 
-# Presi gli autori con h-index > X, quando sono stati realizzati gli articoli che influiscono sull’h-index? In quale momento della carriera?
-@app.route('/api/h_index_career')
-def h_index_career(h_index = 0):
+    return analyses_obj.correlation_between_hindex_and_excluded_articles()
 
-    h_index_threshold = request.args.get('h-index', default=h_index, type=int)
 
-    filter={
-        '$expr': {
-            '$gte': [
-                '$h-index',
-                h_index_threshold
-            ]
-        }
-    }
+# Is there a relationship between h-index and the number of years in one's career?
+@app.route('/api/correlation_between_hindex_and_career_duration')
+def correlation_between_hindex_and_career_duration_api():
+    analyses_obj = analyses(mongo)
 
-    project={
-        '_id': 0, 
-        'articles': -1,
-        'h-index': -1 ,
-        'author-profile.publication-range': -1
-    }
-    df_filtered = mongo.db.collectionAuthorsAggregate.find(filter=filter,projection=project)
-    df_filtered = pd.DataFrame(list(df_filtered))
+    return obj.correlation_between_hindex_and_career_duration()
 
-    def extract_data(row):
-        h_index = int(row['h-index'])
-        career_start_year = int(row['author-profile']['publication-range']['@start'])
-        career_end_year = int(row['author-profile']['publication-range']['@end'])
-        df_abstracts = pd.DataFrame()
 
-        for type in ['main_author', 'coauthor']:
-            if row.articles[type] is not None:
-                df_abstracts = pd.concat([df_abstracts, pd.DataFrame(row.articles[type])[['coverDate', 'citedby_count']]], ignore_index=True)
+# Given a set of authors with an h-index > X, is there a correlation between the number of citations 
+# of an abstract and the conference ranking?
+@app.route('/api/citation_count_based_on_conference_ranking')
+def citation_count_based_on_conference_ranking_api():
+    h_index_threshold = request.args.get('h-index', default=0, type=int)
+    analyses_obj = analyses(mongo, h_index_threshold)
 
-        if not df_abstracts.empty:
-            df_abstracts = df_abstracts.sort_values('citedby_count', ascending=False).head(h_index)
+    return analyses_obj.citation_count_based_on_conference_ranking()
 
-            df_abstracts['coverYear'] = pd.to_datetime(df_abstracts['coverDate'])
-            df_abstracts['years_since_career_start'] = df_abstracts['coverYear'].dt.year - career_start_year
-            max_difference = career_end_year - career_start_year
-            df_abstracts['coefficient'] = df_abstracts['years_since_career_start'] / max_difference
-            return df_abstracts['coefficient'].mean()
-
-        return None
-
-    df_filtered = df_filtered.apply(lambda row : extract_data(row), axis=1)
-    df_filtered = df_filtered.dropna()
-
-    return dumps(
-        {
-            'weight_career_avg': df_filtered.mean(),
-            'weight_career_data': loads(df_filtered.to_json(orient='records'))
-        }
-    )
-
-# Dato un insieme di autori con h-index > X, la variazione tra h-index e articoli non presi in considerazione per il
-# calcolo dell'indice determinando se vi è anche una correlazione.
-@app.route('/api/abstracts_outside_h_index')
-def abstracts_outside_h_index(h_index = 0):
-
-    h_index_threshold = request.args.get('h-index', default=h_index, type=int)
-
-    filter={
-        '$expr': {
-            '$gte': [
-                '$h-index',
-                h_index_threshold
-            ]
-        }
-    }
-
-    project={
-        '_id': 0, 
-        'h-index': -1,
-        'document-count': '$coredata.document-count'
-    }
-    df_filtered = mongo.db.collectionAuthorsAggregate.find(filter=filter,projection=project)
-    df_filtered = pd.DataFrame(list(df_filtered))
-
-    return dumps(
-        {
-            'correlation': df_filtered['h-index'].corr(df_filtered['document-count'])
-        }
-    )
-
-# Vi è una relazione tra h-index e numero di anni di carriera?
-@app.route('/api/corr_career_h_index')
-def corr_career_h_index():
-
-    filter={
-        'author-profile.publication-range': {
-            '$ne': None
-        }
-    }
-
-    project={
-        '_id': 0, 
-        'h-index': -1,
-        'end': '$author-profile.publication-range.@end',
-        'start': '$author-profile.publication-range.@start'
-    }
-    df_filtered = mongo.db.collectionAuthorsAggregate.find(projection=project, filter=filter)
-    df_filtered = pd.DataFrame(list(df_filtered))
-
-    df_filtered['duration_career'] = df_filtered['end'].astype(int) - df_filtered['start'].astype(int)
-
-    return dumps(
-        {
-            'correlation': df_filtered['h-index'].corr(df_filtered['duration_career'])
-        }
-    )
-
-# Dato un insieme di autori con h-index > X, come varia il numero di citazioni di un autore in base al ranking della conferenza
-@app.route('/api/corr_rating_citedby')
-def corr_rating_citedby(h_index = 0):
-
-    h_index_threshold = request.args.get('h-index', default=h_index, type=int)
-
-    filter={
-        '$expr': {
-            '$gte': [
-                '$h-index',
-                h_index_threshold
-            ]
-        }
-    }
-
-    project={
-        '_id': 0, 
-        'h-index': -1,
-        'articles': -1
-    }
-    df_filtered = mongo.db.collectionAuthorsAggregate.find(filter=filter,projection=project)
-    df_filtered = pd.DataFrame(list(df_filtered))
-
-    def extract_data(row):
-        df_abstracts = pd.DataFrame()
-
-        for type in ['main_author', 'coauthor']:
-            if row.articles[type] is not None:
-                df = pd.DataFrame(row.articles[type])
-                df = df[df['GGS_Rating'].notna()]
-                df_abstracts = pd.concat([df_abstracts, df], ignore_index=True)
-
-        return df_abstracts
-
-    df_filtered = df_filtered.apply(lambda row : extract_data(row), axis=1)
-    df_filtered = pd.concat(df_filtered.tolist(), ignore_index=True)
-
-    return dumps(
-        {
-            'correlation': df_filtered['GGS_Rating'].astype('category').cat.codes.corr(df_filtered['citedby_count']),
-            'avg_by_rating': loads(df_filtered.groupby(by='GGS_Rating', as_index=False)['citedby_count'].mean().to_json(orient='records'))
-        }
-    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('FLASK_SERVER_PORT', 9091), debug=True)
